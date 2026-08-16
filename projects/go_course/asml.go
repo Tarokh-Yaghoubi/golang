5c528c90fae73d1cb76532dd5be9f5cfd4f1ca1f
yaghoubi <tarokh.yaghoubi@ENIAC-TECH.COM>
2026-08-16 14:10:31 +0330

    working with arrays

--- projects/go_course/asml.go
+++ projects/go_course/asml.go
@@ -2,7 +2,9 @@ package main
 
 import "fmt"
 
-func check() rune {
-
-	return 'f'
+func arrays() {
+	var intArr [3]int32;
+	fmt.Println(intArr[0])
+	fmt.Println(intArr[1:3])
+	fmt.Println(intArr)
 }
