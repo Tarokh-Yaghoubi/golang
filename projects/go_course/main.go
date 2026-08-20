7f959bd0f4877630bffdb438b95fb7768848e495
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2026-08-20 23:10:34 +0330

    added the test string function

--- projects/go_course/main.go
+++ projects/go_course/main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
-import "unicode/utf8"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
@@ -11,7 +13,6 @@ func main() {
 
 	var floatNumber float64 = 432.543
 	fmt.Println(floatNumber)
-
 
 	var floatNum32 float32 = 11.2
 	var intNum32 int32 = 4
@@ -33,24 +34,22 @@ func main() {
 	fmt.Println(anotherString)
 	fmt.Println(concatString)
 
-	fmt.Println(len("µ"))	// in go, len returns the number of bytes not the number of characters, so this will return 2
+	fmt.Println(len("µ")) // in go, len returns the number of bytes not the number of characters, so this will return 2
 	// not 1
 
-
 	// "RuneCountInString" function in unicode/utf8 package will return the number of characters, and not the bytes
-	fmt.Println(utf8.RuneCountInString("µµµµ"))	// 4
-	
+	fmt.Println(utf8.RuneCountInString("µµµµ")) // 4
 
 	var myRune rune = 't'
-	fmt.Println(myRune)	// this will return the ascii code (dec) - 116
+	fmt.Println(myRune) // this will return the ascii code (dec) - 116
 
-	var myCondition bool = false	// booleans in golang
+	var myCondition bool = false // booleans in golang
 	var yourCondition bool = true
-	
+
 	fmt.Println(myCondition)
 	fmt.Println(yourCondition)
 
-	var myInteger int	// automatically initializes to zero for INTEGERS
+	var myInteger int // automatically initializes to zero for INTEGERS
 	fmt.Println(myInteger)
 
 	// these are other ways to define a variable in golang
@@ -63,13 +62,13 @@ func main() {
 	first, second := 25, 26
 	fmt.Println(first)
 	fmt.Println(second)
-	
+
 	// this is a const, u cannot change it later
 	// you cannot keep it uninitialized
-	const myconst string  = "this is a const value"
+	const myconst string = "this is a const value"
 	fmt.Println(myconst)
 
 	// test_functions()
-	arrays()
+	// arrays()
+	testStrings()
 }
-
