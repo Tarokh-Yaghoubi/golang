f20cd7199d9f693a0b23d60ee254bbf0514b6e9a
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2026-09-10 22:34:52 +0330

    variables in golang, variable packing, declaration and definition and so on

--- projects/go_course/str.go
+++ projects/go_course/str.go
@@ -5,10 +5,22 @@ import (
 	"strings"
 )
 
+// this is how we group multiple variables in golang
+
+// this can be used for global variables, 
+var (
+	myInt int = 43
+	myString string = "Hello Golang"
+	myBool bool = false
+	myFloat float64 = 3.14
+)
+// this can be defined inside a function as well
+
 func testStrings() {
 	var myRune = 'a'
 	fmt.Printf("my rune = %c\n", myRune)
-
+	var apartmentNumber, streetname = 54, "Main Street"
+	apartmentNumber2, streetname2 := 54, "Main Street" // this is also possible 
 	var strSlice = []string{"t", "a", "r", "o", "k", "h"}
 	var strBuilder strings.Builder
 	for i := range strSlice {
@@ -35,6 +47,29 @@ func testStrings() {
 	// or this:
 	fullname = fmt.Sprintf("%s %s", firstname, lastname)
 	fmt.Printf("fullname is -> %s\n", fullname)
+
+
+	// if u wanna declare a variable, this is how to do:
+	//var nickname string
+	// then u can define it later:
+	//nickname = "Craw"
+
+	// but if u wanna define the variable, 
+	// u can just omit the var keyword and use := operator:
+	//otherName := "Craw"	// no var keyword
+	
+
+	// u can also use the old format:
+	//var anothername string = "OtherName - Craw Again"
+	//fmt.Printf("anothername is -> %s\n", anothername)
+
+	// var age int
+	// var age2 int = 22
+	// var age3 = 22	// this is also possible
+	// var age4, othername2 = 22, "Craw"	// this is also possible
+	// age5 := 22	// this is also possible
+	age6, othername3 := 22, "Craw"	// also this one :)
+	fmt.Printf("age and other name values => %v %s\n", age6, othername3)
 }
 
 func main() {
