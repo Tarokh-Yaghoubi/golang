965d58d87ce7d19c4597316867222f3a65478f1d
Tarokh Yaghoubi <tarokhyaghoubi@protonmail.com>
2026-09-17 13:52:40 +0330

    strings and runes in golang

--- projects/go_course/strings_and_runes.go
+++ projects/go_course/strings_and_runes.go
@@ -2,8 +2,7 @@ package main
 
 import "fmt"
 
-
-// strings are stored as sequences of bytes, 
+// strings are stored as sequences of bytes,
 // each character can represent 1, 2 or more bytes of data
 // The slice expression works with strings
 // string, rune, byte types can be converted to each other
@@ -16,13 +15,13 @@ func test() {
 
 	var s string = "Code & Learn"
 	fmt.Println(string(s[0]))
-	fmt.Println(s[0])	// this will also work, but it will just print out the ASCII Numeric of the character, not its string representation
+	fmt.Println(s[0]) // this will also work, but it will just print out the ASCII Numeric of the character, not its string representation
 	fmt.Println(string(s[:5]))
 	fmt.Println(string(s[3:]))
 	fmt.Println(string(s[:]))
 	// s[1] = 'a'	// this is a bug
 
-	name := "tarokh"	// sequence of bytes
+	name := "tarokh" // sequence of bytes
 	// firstLetter := name[0]
 	// fmt.Println(firstLetter)
 	// lastLetter := name[len(name) - 1]
@@ -39,11 +38,10 @@ func test() {
 	runeName := []rune(name)
 	fmt.Println(runeName)
 
-
 	solKey := "𝄞𝄞𝄞"
-	unicodeString := []rune(solKey)	// first we will convert the string into a rune-array
-	firstCharacter := unicodeString[0]	// we will fetch the first rune from the unicodeString
-	fmt.Println(string(firstCharacter))	// we will print it out, so it will not be garbage
+	unicodeString := []rune(solKey)     // first we will convert the string into a rune-array
+	firstCharacter := unicodeString[0]  // we will fetch the first rune from the unicodeString
+	fmt.Println(string(firstCharacter)) // we will print it out, so it will not be garbage
 }
 
 func main() {
